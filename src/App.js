bfc5b6d32110acf9e1899e2f0c7f81af37856f2c
liling0726 <1329301856@qq.com>
2019-01-17 14:21:55 +0800

    setState

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      test:'initailState'
+    }
+  }
+  handleClick=()=>{
+    this.setState({
+      test:'handleClick'
+    })
+    console.log(this.state.test)
+  }
+  componentWillMount(){
+    this.setState({
+      test:'componentWillMount'
+    })
+    console.log(this.state.test)
+  }
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        test:'componentDidMount setTimeout'
+      })
+      console.log(this.state.test)
+    })
+  }
   render() {
+    
     // this.props.name='nike'
     //console.log(this.props.key)
+    const { test } = this.state
+    console.log(test)
     return (
-      <div className="App" ref="name" >
-        123{this.props.name}
+      <div className="App" ref="name"  onClick={this.handleClick}>
+        123{this.props.name}{test}
         {/* <Logo ref={this.refs.name}>
         {this.props.children}
         </Logo> */}
